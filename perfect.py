5cc0f8ec88bb884f28b320abc2225b24cd9251bd
nathan <natebrooks004@gmail.com>
2020-04-08 18:26:17 -0500

    Fixed bug where kruskal's algorithm didn't have horizontals on final row

--- perfect.py
+++ perfect.py
@@ -132,11 +132,11 @@ def kruskal(y_max, x_max):
         parent[find(x)] = find(y)
     
     # shuffled list of walls
-    walls = list()
-    for y in range(1, y_max - 2, 2):
-        walls.extend((y, x) for x in range(2, x_max - 1, 2))
-        y += 1
+    walls = [(1, x) for x in range(2, x_max - 1, 2)]
+    for y in range(2, y_max - 2, 2):
         walls.extend((y, x) for x in range(1, x_max, 2))
+        y += 1
+        walls.extend((y, x) for x in range(2, x_max - 1, 2))
     shuffle(walls)
     
     # join cells adjacent to wall if they are in different sets
